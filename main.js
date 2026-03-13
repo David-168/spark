27b155c5c1b7ea652af9b6ed2e5cf58390681de6
SungYang Wu <david.wu@zenplustek.com>
2026-03-13 16:56:52 +0800

    Check

--- main.js
+++ main.js
@@ -368,14 +368,14 @@ console.log(splatconfig.backgroundColor);    // "#000000"
   butterfly.quaternion.set(0, 0, 0, 0);
   butterfly.position.set(splatconfig.position[0],
      splatconfig.position[1], splatconfig.position[2]);
-  butterfly.rotation.set((
-    splatconfig.rotation[0] * Math.PI/180,
-    splatconfig.rotation[1] * Math.PI/180,
-    splatconfig.rotation[2] * Math.PI/180
-  ));
+ // butterfly.rotation.set((
+//    splatconfig.rotation[0] * Math.PI/180,
+//    splatconfig.rotation[1] * Math.PI/180,
+//    splatconfig.rotation[2] * Math.PI/180
+//  ));
   scene.add(butterfly);
   renderer.setClearColor(0x000000, 1)
-  butterfly.scale.set(splatconfig.scale[0], splatconfig.scale[1], splatconfig.scale[2]);
+//  butterfly.scale.set(splatconfig.scale[0], splatconfig.scale[1], splatconfig.scale[2]);
   renderer.setAnimationLoop(function animate(time) {
     renderer.render(scene, camera);
 //    butterfly.rotation.y += 0.01;
